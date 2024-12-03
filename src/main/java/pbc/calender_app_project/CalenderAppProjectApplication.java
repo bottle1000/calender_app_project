65941c42f075026f4812e1b43ffb810e47c07440
박병천 <genius178896@gmail.com>
2024-12-03 11:59:30 +0900

    Initial commit

---
+++ src/main/java/pbc/calender_app_project/CalenderAppProjectApplication.java
@@ -0,0 +1,13 @@
+package pbc.calender_app_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalenderAppProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalenderAppProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pbc/calender_app_project/CalenderAppProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package pbc.calender_app_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalenderAppProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
