bdc1f3a05d1eaf7fa24285b3a899df8b16d8c3e2
박병천 <genius178896@gmail.com>
2024-12-03 15:42:57 +0900

    feat : 일정 생성 repository

--- src/main/java/pbc/calender_app_project/repository/CalenderRepository.java
+++ src/main/java/pbc/calender_app_project/repository/CalenderRepository.java
@@ -1,4 +1,9 @@
 package pbc.calender_app_project.repository;
 
+import pbc.calender_app_project.dto.CalenderResponseDto;
+import pbc.calender_app_project.entity.Calender;
+
 public interface CalenderRepository {
+
+    CalenderResponseDto createCalender(Calender calender);
 }
--- src/main/java/pbc/calender_app_project/repository/JdbcTemplateCalenderRepository.java
+++ src/main/java/pbc/calender_app_project/repository/JdbcTemplateCalenderRepository.java
@@ -1,7 +1,15 @@
 package pbc.calender_app_project.repository;
 
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
+import pbc.calender_app_project.dto.CalenderResponseDto;
+import pbc.calender_app_project.entity.Calender;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
 
 @Repository
 public class JdbcTemplateCalenderRepository implements CalenderRepository {
@@ -11,5 +19,20 @@ public class JdbcTemplateCalenderRepository implements CalenderRepository {
         this.jdbcTemplate = jdbcTemplate;
     }
 
+    @Override
+    public CalenderResponseDto createCalender(Calender calender) {
 
+        SimpleJdbcInsert jdbcInsert = new SimpleJdbcInsert(jdbcTemplate);
+        jdbcInsert.withTableName("calender").usingGeneratedKeyColumns("id");
+
+        Map<String, Object> parameters = new HashMap<>();
+        parameters.put("todo_list", calender.getToDoList());
+        parameters.put("name", calender.getName());
+        parameters.put("password", calender.getPassword());
+        parameters.put("created_at", LocalDateTime.now());
+        parameters.put("updated_at", LocalDateTime.now());
+
+        Number key = jdbcInsert.executeAndReturnKey(new MapSqlParameterSource(parameters));
+        return new CalenderResponseDto(key.longValue(), calender.getToDoList(), calender.getName(), calender.getPassword());
+    }
 }
