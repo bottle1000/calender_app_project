783dd2daba249f2b13e3aa8dc87caa535dac13a8
박병천 <genius178896@gmail.com>
2024-12-03 12:09:58 +0900

    chore : create entity class

--- src/main/java/pbc/calender_app_project/entity/Calender.java
+++ src/main/java/pbc/calender_app_project/entity/Calender.java
@@ -1,4 +1,17 @@
 package pbc.calender_app_project.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@AllArgsConstructor
 public class Calender {
+
+    private String toDoList;
+    private String name;
+    private String password;
+    private LocalDateTime writeDate;
+    private LocalDateTime updateDate;
 }
