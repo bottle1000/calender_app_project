25ba9c605d76a45bac89a696b0c326e9c8f4bbeb
박병천 <genius178896@gmail.com>
2024-12-03 15:43:38 +0900

    chore : 필드 id 값 추가

--- src/main/java/pbc/calender_app_project/dto/CalenderResponseDto.java
+++ src/main/java/pbc/calender_app_project/dto/CalenderResponseDto.java
@@ -9,10 +9,17 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 public class CalenderResponseDto {
 
+    private Long id;
     private String toDoList;
     private String name;
     private String password;
     private LocalDateTime writeDate;
     private LocalDateTime updateDate;
 
+    public CalenderResponseDto(Long id, String toDoList, String name, String password) {
+        this.id = id;
+        this.toDoList = toDoList;
+        this.name = name;
+        this.password = password;
+    }
 }
