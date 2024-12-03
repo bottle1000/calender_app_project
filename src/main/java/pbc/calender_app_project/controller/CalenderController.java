567ddd7f241975c73dac367fc2d1474b366c7e9c
박병천 <genius178896@gmail.com>
2024-12-03 15:42:16 +0900

    feat : 일정 생성 controller

--- src/main/java/pbc/calender_app_project/controller/CalenderController.java
+++ src/main/java/pbc/calender_app_project/controller/CalenderController.java
@@ -1,8 +1,13 @@
 package pbc.calender_app_project.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pbc.calender_app_project.dto.CalenderRequestDto;
+import pbc.calender_app_project.dto.CalenderResponseDto;
 import pbc.calender_app_project.service.CalenderService;
 
 @RestController
@@ -15,5 +20,10 @@ public class CalenderController {
         this.calenderService = calenderService;
     }
 
+    @PostMapping
+    public ResponseEntity<CalenderResponseDto> createCalender(@RequestBody CalenderRequestDto dto) {
+
+        return new ResponseEntity<>(calenderService.createCalender(dto), HttpStatus.CREATED);
+    }
 
 }
